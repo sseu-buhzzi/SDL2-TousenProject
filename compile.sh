6b3ea6f8af4c0d69383e3fc782796725cb17e684
xeu <xeu@debian>
2025-05-24 23:00:07 +0800

    Rename Touken to Tousen
    當時尖團不分、結果把「綫」sen當成ken了。

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 cd "$(dirname "$0")"
 mkdir -p build
-g++ ToukenProject.cpp $(sdl2-config --cflags --libs) -lGL -lGLU -o build/TousenProject
+g++ TousenProject.cpp $(sdl2-config --cflags --libs) -lGL -lGLU -o build/TousenProject
 ln -fs TousenProject build/東綫ПРОЖЕКТ
